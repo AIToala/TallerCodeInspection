79f30512e6ff3aa116908380a751706f9fcf89a8
AIToala <aitoala@espol.edu.ec>
2023-06-07 16:00:26 -0500

    Work made in class - dev section

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -2,11 +2,11 @@ package labcodeinspection;
 
 public class Email {
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
+	private String m_firstName; //NOPMD This field will be manipulated later
+	private String m_lastName; //NOPMD This field will be manipulated later
+	private String password = "";
 	private String department;
-	private int defaultpasswordLength = 8;
+	private final int defaultpasswordLength = 8;
 	private String email;
 
 	public Email(String firstName, String lastName) {
@@ -30,7 +30,9 @@ public class Email {
 		case 3:
 			this.department = "acct";
 			break;
-		}
+		default:
+			break;
+		}	
 	}
 
 	private String randomPassword(int length) {
@@ -40,7 +42,7 @@ public class Email {
 			int rand = (int) (Math.random() * set.length());
 			password[i] = set.charAt(rand);
 		}
-		return new String(password);
+		return password.toString();
 	}
 
 	public void generateEmail() {
